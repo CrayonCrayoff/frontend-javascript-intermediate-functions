765fcdf6f1239821fa6f04e5d891fbe5c2928c52
CrayonCrayoff <krejt90@gmail.com>
2026-03-24 16:02:32 +0100

    Uitwerking opdracht 1 (functions-methods)

--- functions-methods.js
+++ functions-methods.js
@@ -9,7 +9,11 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+function getEmailDomain(email) {
+    return email.substring(email.indexOf("@"), email.length);
+}
 
+console.log(getEmailDomain("n.eeken@novi-education.nl"));
 
 
 /* Opdracht  2 */
