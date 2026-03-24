724aa6e2129d055122b92483615666539b9fc7e0
CrayonCrayoff <krejt90@gmail.com>
2026-03-24 15:47:19 +0100

    Uitwerking opdracht 1a

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -16,6 +16,16 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 6
 
+let numberOfCumLaude = 0;
+
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 8) {
+        numberOfCumLaude++;
+    }
+}
+
+console.log(numberOfCumLaude);
+
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
